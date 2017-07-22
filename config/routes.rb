81cd08b92fa8e59eed6d3eeff5dd9dae583d726b
probro <dev@pellue.com>
2017-07-22 12:41:06 +0900

    add register and login view and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
+# Rails.application.routes.draw do
+#   get 'user_sessions/new'
+#   get 'user_sessions/create'
+#   get 'user_sessions/destroy'
+#
+#   resources :users
+#   resources :posts
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# end
+
 Rails.application.routes.draw do
+  root 'users#index'
+
+  resources :user_sessions
   resources :users
   resources :posts
+
+  get 'login' => 'user_sessions#new', as: :login
+  get 'logout' => 'user_sessions#destroy', as: :logout
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
